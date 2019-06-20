4798c32f10f56d6299a91f1b68e951476ee64b9c
MrTroble <nifroh56@gmail.com>
2019-06-20 23:07:31 +0200

    Changed to true!

--- login.php
+++ login.php
@@ -12,11 +12,11 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://github.com/login/oauth/access_token");
 curl_setopt($curl, CURLOPT_POST , 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, "client_id=68f267e2a51c384bff92&client_secret=$secret&code=$code");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $rtn = curl_exec($curl);
 curl_close($curl);
-
+var_dump($rtn);
 if($rnt !== false){
     $token = str_replace("access_token=", "", explode("&", $rnt)[0]);
     $exp = time() + 60 * 60 * 24;
