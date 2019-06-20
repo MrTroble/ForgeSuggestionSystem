55e72180401639d09a75eefded62a938a5839ba2
MrTroble <nifroh56@gmail.com>
2019-06-20 21:40:20 +0200

    Dump

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 var_dump($_POST);
 if(!isset($_POST) || !isset($_POST["code"])){
-    header("Location: https://github.com/login/oauth/authorize?client_id=68f267e2a51c384bff92");
-    die();
+    //die();
 }
 ?>
