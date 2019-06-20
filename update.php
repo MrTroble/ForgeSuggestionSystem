eb99c555d9583302375ab4cd63e2a42818910ce8
MrTroble <nifroh56@gmail.com>
2019-06-20 17:10:52 +0200

    Fixing issue

--- update.php
+++ update.php
@@ -6,10 +6,12 @@ $GLOBALS["params"] = readAndPack("params.csv");
 $GLOBALS["fields"] = readAndPack("fields.csv");
 $file = fopen("cache.html", "w");
 if($file){
-    fwrite($file, "<link rel='stylesheet' type='text/css' href='main.css'>");
+    fwrite($file, "<head><link rel='stylesheet' type='text/css' href='main.css'></head>");
+    fwrite($file, "<body>");
     fwrite($file, display($GLOBALS["methods"]));
     fwrite($file, display($GLOBALS["params"]));
     fwrite($file, display($GLOBALS["fields"]));
+    fwrite($file, "</body>");
     fclose($file);
 } else {
     echo "Error reading write!";
