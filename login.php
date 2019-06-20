2557a06fafd4975ba9878b1048b738200ab49100
MrTroble <nifroh56@gmail.com>
2019-06-20 22:53:36 +0200

    set return type!

--- login.php
+++ login.php
@@ -12,11 +12,12 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://github.com/login/oauth/access_token");
 curl_setopt($curl, CURLOPT_POST , 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, "client_id=68f267e2a51c384bff92&client_secret=$secret&code=$code");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 $rtn = curl_exec($curl);
 curl_close($curl);
 
-if(isset($rnt)){
+if($rnt !== false){
     $token = str_replace("access_token=", "", explode("&", $rnt)[0]);
     $exp = mktime();
     setcookie("token", $token, $exp);
