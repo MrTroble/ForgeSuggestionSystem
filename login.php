b480e45bd0110094a698a5d5df3104ad79834b12
MrTroble <nifroh56@gmail.com>
2019-06-20 21:37:55 +0200

    Added debugging out

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
-if(!isset($_POST) || !isset($_POST["client_secret"])){
+var_dump($_POST);
+if(!isset($_POST) || !isset($_POST["code"])){
     header("Location: https://github.com/login/oauth/authorize?client_id=68f267e2a51c384bff92");
     die();
 }
